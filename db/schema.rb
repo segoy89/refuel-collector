11f414116989d6eadb7468ead75dcc66f5b5d19b
segoy89 <a.szamansky@gmail.com>
2020-08-17 14:46:45 +0200

    Remove confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -33,15 +33,10 @@ ActiveRecord::Schema.define(version: 2020_08_16_081145) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "name"
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     name                    { 'Johny' }
     password                { 'password' }
     password_confirmation   { 'password' }
-    confirmed_at            { Date.current }
 
     sequence(:email) { |n | "john.doe#{n}@example.com" }
   end
