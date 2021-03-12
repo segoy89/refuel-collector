c7744942de66898f30a38a0b19f0e318c3f20b1e
segoy89 <a.szamansky@gmail.com>
2021-03-12 15:04:40 +0100

    Bump puma, adjust devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[password]
   root to: 'home#index'
 
   resources :refueling, except: %i[index show], controller: 'refuelings'
