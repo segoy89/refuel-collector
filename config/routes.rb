404a5d77306e67e92c5c89c1167661089c15b8b7
segoy89 <a.szamansky@gmail.com>
2021-04-08 17:57:36 +0200

    Create api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: '', controllers: { sessions: 'api/v1/sessions' }
-      resources :refuelings
+      resources :refuelings, except: %i[new show edit]
     end
   end
 end
